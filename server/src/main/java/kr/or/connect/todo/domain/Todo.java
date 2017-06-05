8fe6073190f2ab4f7df5b163e8ea8b0e8b6d07fc
Taemin Kim <taemin3000@naver.com>
2017-06-05 15:24:36 +0900

    refine about first commit

--- server/src/main/java/kr/or/connect/todo/domain/Todo.java
+++ server/src/main/java/kr/or/connect/todo/domain/Todo.java
@@ -10,33 +10,43 @@ public class Todo {
     private String todo;
     private String date;
 
-    @Override
-    public String toString() {
-        return "Todo [id=" + id + ", completed=" + completed + ", todo=" + todo + ", date=" + date + "]";
-    }
-
-    public Todo() {}
-
     public Todo(Integer id, Integer completed, String todo, String date) {
         this.id = id;
         this.completed = completed;
         this.todo = todo;
         this.date = date;
     }
+    public Todo() {}
 
-    public Todo(Integer completed, String todo, String date) {
-        this.completed = completed;
-        this.todo = todo;
-        this.date = date;
+    public Integer getId() {
+        return id;
     }
 
-    public Integer getId() {return id;}
-    public void setId(Integer id) {this.id = id;}
-    public Integer getCompleted() {return completed;}
-    public void setCompleted(Integer completed) {this.completed = completed;}
-    public String getTodo() {return todo;}
-    public void setTodo(String todo) {this.todo = todo;}
-    public String getDate() {return date;}
-    public void setDate(String date) {this.date = date;}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public Integer getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Integer completed) {
+        this.completed = completed;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
